03e8b08f118b004bb7fd79909c0d733f16e8a5e3
alejandro-gonzales <alejandro.g280211@gmail.com>
2021-05-04 18:52:02 -0400

    WIP: added template to person list

--- resources/views/admin/persona/index.blade.php
+++ resources/views/admin/persona/index.blade.php
@@ -1,49 +1,33 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Personas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
-<style>
-    .w-5{
-        width: 24px;
-    }
-    .h-5{
-        height: 24px;
-    }
-</style>
-<body>
-    <div class="container"><br>
-        <h3>LISTA DE PERSONAS</h3><br>
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>CI</th>
-                    <th>Celular</th>
-                    <th>Dirección</th>
-                    <th>Opciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($personas as $persona)
-                <tr>
-                    <td>{{$persona->nombre}}</td>
-                    <td>{{$persona->apellido}}</td>
-                    <td>{{$persona->ci}}</td>
-                    <td>{{$persona->celular}}</td>
-                    <td>{{$persona->direccion}}</td>
-                    <td>Opciones</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+@extends('template.main.index')
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/custom-style.css')}}">
+@endSection
+@section('icon', 'users')
+@section('title', 'Gestionar Personas')
+@section('content')
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>CI</th>
+                <th>Celular</th>
+                <th>Dirección</th>
+                <th>Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($personas as $persona)
+            <tr>
+                <td>{{$persona->nombre}}</td>
+                <td>{{$persona->apellido}}</td>
+                <td>{{$persona->ci}}</td>
+                <td>{{$persona->celular}}</td>
+                <td>{{$persona->direccion}}</td>
+                <td>Opciones</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
     {{$personas->render()}}
-    </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-</html>
+@endSection
